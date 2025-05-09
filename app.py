1989254b71e534bcb9d3a1dc1a18d57eb7f7da20
henrique18yh <joaoclashroyale356@gmail.com>
2025-05-09 15:53:40 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+média1 = input("digite o nome do aluno: ") #este codigo calcula o 1°aluno
+
+nota1 = float(input("didgite a primeira nota" )) #coloquei a nota 1
+nota2 = float(input("digite a segunda nota")) #coloquei a nota 2
+nota3 = float(input("digite a terceira nota")) #coloquei a nota 3
+média = (nota1 + nota2 + nota3) / 3 #aqui eu coloquei a soma da conta
+print(f"a media do aluno é: {média:.5f} ") #e aqui eu coloquei o tanto de casa da virgula
